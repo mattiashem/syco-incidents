72a2be571861bbb4b395b5b1fc216e400a213485
Mattias Hemmingsson <mathem@fareoffice.com>
2014-09-24 18:39:52 +0200

    Updated

--- themail.py
+++ themail.py
@@ -14,17 +14,82 @@ def hello():
 
 @app.route("/fp")
 def fp():
-    result = send_campain()
+    result = "Error"
     return render_template('fp.html',result=result)
 
-@app.route("/fp-ok")
+@app.route("/fp_ok")
 def fp_ok():
-    result = send_campain_resolved()
+    result = "Error"
     return render_template('fp.html',result=result)
 
 
+@app.route("/rf")
+def rf():
+    result = "Error"
+    return render_template('fp.html',result=result)
+
+@app.route("/rf_ok")
+def rf_ok():
+    result = "Error"
+    return render_template('fp.html',result=result)
+
+@app.route("/eff")
+def eff():
+    result = "Error"
+    return render_template('fp.html',result=result)
+
+@app.route("/eff_ok")
+def eff_ok():
+    result = "Error"
+    return render_template('fp.html',result=result)
 
 
+@app.route("/pbk")
+def pbk():
+    result = "Error"
+    return render_template('fp.html',result=result)
+
+@app.route("/pbk_ok")
+def pbk_ok():
+    result = "Error"
+    return render_template('fp.html',result=result)
+
+
+@app.route("/webres")
+def webres():
+    result = "Error"
+    return render_template('fp.html',result=result)
+
+@app.route("/webres_ok")
+def webres_ok():
+    result = "Error"
+    return render_template('fp.html',result=result)
+
+@app.route("/static")
+def static_e():
+    result = "Error"
+    return render_template('fp.html',result=result)
+
+@app.route("/static_ok")
+def static_ok():
+    result = "Error"
+    return render_template('fp.html',result=result)
+
+@app.route("/all")
+def all():
+    result = "Error"
+    return render_template('fp.html',result=result)
+
+@app.route("/all_ok")
+def all_ok():
+    result = "Error"
+    return render_template('fp.html',result=result)
+
+
+@app.route("/panic")
+def panic():
+    result = "Error"
+    return render_template('fp.html',result=result)
 
 #Create an campain
 def send_campain():
